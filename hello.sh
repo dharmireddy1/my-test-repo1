26bb7c3376f141cb1bcd644440baed711c424754
dharmireddy1 <dharmireddy.durga@gmail.com>
2025-11-16 21:37:44 -0600

    add name parameter to hello.sh

--- hello.sh
+++ hello.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env bash
-# author prasad
-# heelo.sh - a simple hello world script
-set -e
-name="prasad"
-echo "hello, $name! this is my first bash script."
-echo "Today's date and time: $(date)"
-echo "this file was created by hello.ss on $(date) " > hello.txt
-echo "done creation of hello.txt file"
+#!/bin/bash
+
+# If no argument given, print usage message
+if [ -z "$1" ]; then
+  echo "Usage: ./hello.sh <your-name>"
+  exit 1
+fi
+
+echo "Hello, $1! Welcome to Bash scripting 🚀"
+
 
 
